c6298a9de60dffbcf020271f6425e9a3af434c35
Ricardo Vieira <ricardomachado03@gmail.com>
2023-11-29 09:35:24 -0300

    implementando uma requisição em js

--- Pokedex/assets/js/main.js
+++ Pokedex/assets/js/main.js
@@ -1,1 +1,14 @@
-console.log("Foi!")
+const offset = 0
+const limit = 10
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
+fetch(url).then(function (response) {
+    console.log(response)
+
+})
+.catch(function (error){
+    console.error(error)
+})
+.finally(function (){
+    console.log('Requisição concluida!')
+})
