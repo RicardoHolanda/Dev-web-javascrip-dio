5ed78e4465d3ace790fb1822890ede3d0d71f024
Ricardo Vieira <ricardomachado03@gmail.com>
2023-11-29 11:32:35 -0300

    mudando a identação

--- Pokedex/assets/js/main.js
+++ Pokedex/assets/js/main.js
@@ -2,10 +2,15 @@ const offset = 0
 const limit = 10
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
-fetch(url).then(function (response) {
-    console.log(response)
+fetch(url)
+    .then(function (response) {
+        response
+            .json()
+            .then(function (responsebody){
+                console.log(responsebody)
+            })
 
-})
+    })
 .catch(function (error){
     console.error(error)
 })
